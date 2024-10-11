456b4b7645a49aba8def30b84b3b271a21c51f8c
Shyanukant Rathi <72182251+shyanukant@users.noreply.github.com>
2024-10-11 04:32:52 +0000

    change endpoint

--- App/Services/GlobalApi.js
+++ App/Services/GlobalApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL='http://192.168.0.113:3000/api/bardapi' //Replace with System PC IP address
+const BASE_URL='https://aurora-api-rttx.onrender.com/api/api' //Replace with System PC IP address
 
 const getBardApi=(userMsg)=>axios.get(BASE_URL+"?ques="+userMsg);
 
